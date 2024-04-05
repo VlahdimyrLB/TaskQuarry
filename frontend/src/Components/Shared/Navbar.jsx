ef0d5ae7de95bea39bd2b5d27a7fbb4ededd87bb
VlahdimyrLB <sgespedillon@gmail.com>
2024-04-06 01:01:57 +0800

    Updated Admin Page and Navbar

---
+++ frontend/src/Components/Shared/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between text-blue-gray-900">
+      <div className="block antialiased font-sans text-base leading-relaxed text-inherit mr-4 cursor-pointer  font-bold">
+        TaskQuarry
+      </div>
+    </div>
+  );
+};
+export default Navbar;
