270ab6e37d1d7aba53396bb8d5902a47d7717a9a
dinininin <nadineordonez.0830@gmail.com>
2024-04-09 20:58:14 +0800

    Fixed dashboard responsiveness

--- frontend/src/Components/Admin/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Admin/Sidebar/Sidebar.jsx
@@ -3,12 +3,14 @@ import {
   AccordionHeader,
   AccordionBody,
   Card,
+  IconButton,
   Typography,
   List,
   ListItem,
   ListItemPrefix,
   ListItemSuffix,
   Chip,
+  Drawer,
 } from "@material-tailwind/react";
 import {
   PresentationChartBarIcon,
@@ -16,105 +18,118 @@ import {
   UserCircleIcon,
   ClockIcon,
   FolderIcon,
-  PowerIcon,
   ChevronRightIcon,
   ChevronDownIcon,
+  Bars3Icon,
+  XMarkIcon,
 } from "@heroicons/react/24/outline";
 import { useState } from "react";
 
-const Sidebar = () => {
+const Sidebar = ({ isOpen, close }) => {
+  // HANDLES STATE FOR TASK MANAGEMENT ACCORDION
   const [open, setOpen] = useState(0);
-
   const handleOpen = (value) => {
     setOpen(open === value ? 0 : value);
   };
+
   return (
-    <div className="hidden h-screen max-w-[16rem] shadow-xl bg-[#1B1B1B] rounded-0 md:block">
-      <div className="flex justify-center items-center h-12 bg-[#282828]">
-        <p className="text-lg text-white font-bold">TaskQuarry</p>
-      </div>
-      <div className="p-2 text-white">
-        <List className="text-white text-sm">
-          <ListItem>
-            <ListItemPrefix>
-              <HomeIcon className="h-5 w-5 " />
-            </ListItemPrefix>
-            Dashboard
-          </ListItem>
-          <Accordion
-            open={open === 1}
-            icon={
-              <ChevronDownIcon
-                strokeWidth={2.5}
-                className={`text-white mx-auto h-4 w-4 transition-transform ${
-                  open === 1 ? "rotate-180" : ""
-                }`}
-              />
-            }
-          >
-            <ListItem className="p-0" selected={open === 1}>
-              <AccordionHeader
-                onClick={() => handleOpen(1)}
-                className="border-b-0 p-3"
-              >
-                <ListItemPrefix>
-                  <PresentationChartBarIcon className="text-white h-5 w-5" />
-                </ListItemPrefix>
-                <Typography color="white" className="mr-auto text-sm">
-                  Task Management
-                </Typography>
-              </AccordionHeader>
+    <>
+      {/* TO DO: ADD ANIMATION FOR SIDEBAR CLOSE/OPEN */}
+      <div
+        id="drawer-example"
+        className={`h-screen max-w-[16rem] shadow-xl bg-[#1B1B1B] rounded-0 md:block ${
+          isOpen
+            ? "block fixed top-0 left-0 z-50 md:static md:h-auto md:max-h-none"
+            : "hidden"
+        }`}
+      >
+        <div className="flex justify-between items-center h-12 bg-[#282828]">
+          <div className="flex ml-[17px]">
+            <p className="text-lg text-white font-bold">TaskQuarry</p>
+          </div>
+          <div className="flex items-center mr-1 md:hidden">
+            <IconButton variant="text" size="md" onClick={close}>
+              <XMarkIcon className="h-7 w-7 stroke-1 text-white" />
+            </IconButton>
+          </div>
+        </div>
+        <div className="p-2 text-white">
+          <List className="text-white text-sm">
+            <ListItem>
+              <ListItemPrefix>
+                <HomeIcon className="h-5 w-5 " />
+              </ListItemPrefix>
+              Dashboard
             </ListItem>
-            <AccordionBody className="py-1">
-              <List className="p-0 text-white text-sm">
-                <ListItem>
+            <Accordion
+              open={open === 1}
+              icon={
+                <ChevronDownIcon
+                  strokeWidth={2.5}
+                  className={`text-white mx-auto h-4 w-4 transition-transform ${
+                    open === 1 ? "rotate-180" : ""
+                  }`}
+                />
+              }
+            >
+              <ListItem className="p-0" selected={open === 1}>
+                <AccordionHeader
+                  onClick={() => handleOpen(1)}
+                  className="border-b-0 p-3"
+                >
                   <ListItemPrefix>
-                    <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                    <PresentationChartBarIcon className="text-white h-5 w-5" />
                   </ListItemPrefix>
-                  Create Task
-                </ListItem>
-                <ListItem>
-                  <ListItemPrefix>
-                    <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                  </ListItemPrefix>
-                  Task List
-                </ListItem>
-                <ListItem>
-                  <ListItemPrefix>
-                    <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                  </ListItemPrefix>
-                  Contributors
-                </ListItem>
-              </List>
-            </AccordionBody>
-          </Accordion>
-          <ListItem>
-            <ListItemPrefix>
-              <UserCircleIcon className="h-5 w-5 " />
-            </ListItemPrefix>
-            User Management
-          </ListItem>
-          <ListItem>
-            <ListItemPrefix>
-              <FolderIcon className="h-5 w-5 " />
-            </ListItemPrefix>
-            Reports
-          </ListItem>
-          <ListItem>
-            <ListItemPrefix>
-              <ClockIcon className="h-5 w-5 " />
-            </ListItemPrefix>
-            History
-          </ListItem>
-          <ListItem>
-            <ListItemPrefix>
-              <PowerIcon className="h-5 w-5 " />
-            </ListItemPrefix>
-            Log Out
-          </ListItem>
-        </List>
+                  <Typography color="white" className="mr-auto text-sm">
+                    Task Management
+                  </Typography>
+                </AccordionHeader>
+              </ListItem>
+              <AccordionBody className="py-1">
+                <List className="p-0 text-white text-sm">
+                  <ListItem>
+                    <ListItemPrefix>
+                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                    </ListItemPrefix>
+                    Create Task
+                  </ListItem>
+                  <ListItem>
+                    <ListItemPrefix>
+                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                    </ListItemPrefix>
+                    Task List
+                  </ListItem>
+                  <ListItem>
+                    <ListItemPrefix>
+                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                    </ListItemPrefix>
+                    Contributors
+                  </ListItem>
+                </List>
+              </AccordionBody>
+            </Accordion>
+            <ListItem>
+              <ListItemPrefix>
+                <UserCircleIcon className="h-5 w-5 " />
+              </ListItemPrefix>
+              User Management
+            </ListItem>
+            <ListItem>
+              <ListItemPrefix>
+                <FolderIcon className="h-5 w-5 " />
+              </ListItemPrefix>
+              Reports
+            </ListItem>
+            <ListItem>
+              <ListItemPrefix>
+                <ClockIcon className="h-5 w-5 " />
+              </ListItemPrefix>
+              History
+            </ListItem>
+          </List>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/Components/Shared/Navbar.jsx
+++ frontend/src/Components/Shared/Navbar.jsx
@@ -1,11 +1,58 @@
-const Navbar = () => {
+import {
+  Avatar,
+  Button,
+  ChevronDownIcon,
+  ListItemPrefix,
+  Menu,
+  MenuItem,
+  MenuHandler,
+  MenuList,
+  Typography,
+} from "@material-tailwind/react";
+import {
+  CubeTransparentIcon,
+  UserCircleIcon,
+  CodeBracketSquareIcon,
+  Square3Stack3DIcon,
+  Cog6ToothIcon,
+  InboxArrowDownIcon,
+  LifebuoyIcon,
+  PowerIcon,
+  RocketLaunchIcon,
+  Bars2Icon,
+} from "@heroicons/react/24/solid";
+import { useState, createElement } from "react";
+import userIcon from "../../Assets/icon.jpg";
+
+const profileMenuItems = [
+  {
+    label: "My Profile",
+    icon: UserCircleIcon,
+  },
+  {
+    label: "Edit Profile",
+    icon: Cog6ToothIcon,
+  },
+  {
+    label: "Sign Out",
+    icon: PowerIcon,
+  },
+];
+
+const Navbar = ({ toggleSidebar }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const closeMenu = () => setIsMenuOpen(false);
+
   return (
-    <nav>
+    <nav className="sticky top-0 left-0 right-0">
       <div className="flex flex-col flex-1 overflow-y-auto">
         <div className="flex items-center justify-between h-12 bg-white border-b border-gray-200">
           <div className="flex items-center px-4">
             {/* HAMBURGER TOGGLE */}
-            <button className="text-gray-500 focus:outline-none focus:text-gray-700 cursor-pointer">
+            <button
+              onClick={toggleSidebar}
+              className="text-gray-500 focus:outline-none focus:text-gray-700 cursor-pointer md:hidden"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6"
@@ -21,7 +68,53 @@ const Navbar = () => {
                 />
               </svg>
             </button>
-            {/* <input className="mx-4 w-full border rounded-md px-4 py-2" type="text" placeholder="Search"> */}
+            <p className="font-bold text-gray-800 ml-2.5">Dashboard</p>
+          </div>
+          <div className="flex space-x-2">
+            <Menu
+              open={isMenuOpen}
+              handler={setIsMenuOpen}
+              placement="bottom-end"
+            >
+              <MenuHandler>
+                <Button
+                  variant="text"
+                  color="blue-gray"
+                  className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
+                >
+                  {/* CAUSES ERROR ELEMENT TYPE NOT VALID:
+                   <ChevronDownIcon
+                    strokeWidth={2.5}
+                    className={`h-3 w-3  ${isMenuOpen ? "rotate-180" : ""}`}
+                  /> */}
+                  <Avatar
+                    variant="circular"
+                    size="sm"
+                    alt="kim chaewon"
+                    className="border border-gray-800 p-[0.75px] mr-2"
+                    src={userIcon}
+                  />
+                </Button>
+              </MenuHandler>
+              <MenuList className="p-1">
+                {profileMenuItems.map(({ label, icon }, key) => {
+                  return (
+                    <MenuItem
+                      key={key}
+                      className="flex items-center gap-2 rounded text-gray-700"
+                    >
+                      {createElement(icon, {
+                        className: `h-4 w-4`,
+                        strokeWidth: 2,
+                      })}
+                      <Typography as="span" variant="small">
+                        {label}
+                      </Typography>
+                    </MenuItem>
+                  );
+                })}
+              </MenuList>
+            </Menu>
           </div>
         </div>
       </div>
