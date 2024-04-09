452caa8013fe7935750b066f6f05efdb3fa7b933
dinininin <nadineordonez.0830@gmail.com>
2024-04-09 23:58:06 +0800

    Added content to Dashboard component and Set the shared prop

--- frontend/src/Pages/AdminPage.jsx
+++ frontend/src/Pages/AdminPage.jsx
@@ -1,29 +1,27 @@
 import { useState } from "react";
 import Sidebar from "../Components/Admin/Sidebar";
 import Navbar from "../Components/Shared/Navbar";
-import { Card } from "@material-tailwind/react";
+import Dashboard from "./AdminPages/Dashboard";
 
 const AdminPage = ({ user }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleCloseSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
     <div className="flex h-screen">
-      <Sidebar />
+      <Sidebar isOpen={isSidebarOpen} close={handleCloseSidebar} />
       <div className="flex flex-col w-full">
-        <Navbar />
+        <Navbar toggleSidebar={toggleSidebar} />
         {/* MAIN CONTENT SECTION */}
         <section className="bg-gray-100 h-screen text-white p-5">
-          {/* TEST DASHBOARD CONTENT NOTE: NOT YET RESPONSIVE */}
-          <p className="font-bold text-gray-800 mb-4">Dashboard</p>
-          <div className="grid grid-cols-1 gap-4 mt-1 md:grid-cols-2 lg:grid-cols-3">
-            <Card className="p-8 rounded-md h-72 mb-2 md:h-[300px] md:w-[400px]">
-              This is a mf card
-            </Card>
-            <Card className="p-8 rounded-md h-72 mb-2 md:h-[300px] md:w-[400px]">
-              This is a mf card
-            </Card>
-            <Card className="p-8 rounded-md h-72 mb-2 md:h-[300px] md:w-[400px]">
-              This is a mf card
-            </Card>
-          </div>
+          <Dashboard />
         </section>
       </div>
     </div>
