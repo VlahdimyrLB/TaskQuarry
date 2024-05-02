0e614264a3f3079bb82c45c101b2faa2a7280d2c
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-05-03 02:59:32 +0800

    hehe

--- frontend/src/Components/Admin/Project/ProjectCard.jsx
+++ frontend/src/Components/Admin/Project/ProjectCard.jsx
@@ -20,23 +20,30 @@ const ProjectCard = ({ project }) => {
           </Typography>
           <p className="mt-2">{project.description}</p>
         </div>
-        <div>
-          <p>
-            Priority:{" "}
-            <span
-              className={`text-white p-2 rounded-full uppercase tracking-[0.13em] font-semibold text-xs ${
-                project.priority === "Urgent"
-                  ? "bg-red-800"
-                  : project.priority === "Important"
-                  ? "bg-deep-orange-500"
-                  : project.priority === "Medium"
-                  ? "bg-blue-800"
-                  : "bg-green-800"
-              }`}
-            >
-              {project.priority}
-            </span>
-          </p>
+        <hr className="px-0 border-2" />
+        <div className="flex flex-row px-4 py-4 justify-between">
+          <div className="flex flex-row space-x-1 items-center">
+            <span className="inline-block h-3 w-3 rounded-full bg-blue-500"></span>
+            <p>In Progress</p>
+          </div>
+          <div>
+            <p>
+              Priority:{" "}
+              <span
+                className={`text-white p-2 rounded-full uppercase tracking-[0.13em] font-semibold text-xs ${
+                  project.priority === "Urgent"
+                    ? "bg-red-800"
+                    : project.priority === "Important"
+                    ? "bg-deep-orange-500"
+                    : project.priority === "Medium"
+                    ? "bg-blue-800"
+                    : "bg-green-800"
+                }`}
+              >
+                {project.priority}
+              </span>
+            </p>
+          </div>
         </div>
       </Card>
       {/* )} */}
